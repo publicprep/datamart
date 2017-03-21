b85636d4b18298048c4f60b7ad385fff6f0f5532
almartin82 <almartin@gmail.com>
2017-03-21 10:40:59 -0400

    test prep views

---
+++ assess/assess$test_prep_ela.sql
@@ -0,0 +1,25 @@
+DROP VIEW assess$test_prep_ela$assessments;
+
+CREATE VIEW assess$test_prep_ela$assessments AS
+SELECT a.assessment_id
+      ,a.academic_year
+      ,a.title
+      ,a.administered_at
+      ,a.tags
+      ,a.performance_band_set_id
+      ,subj.code_translation AS subject_area
+      ,scope.code_translation AS scope
+      ,u.state_id
+  FROM il_dna_assessments.assessments a
+  JOIN il_public.users u
+    ON a.user_id = u.user_id
+  LEFT OUTER JOIN il_codes.dna_subject_areas subj
+    ON a.code_subject_area_id = subj.code_id
+  LEFT OUTER JOIN il_codes.dna_scopes scope
+    ON a.code_scope_id = scope.code_id
+ WHERE a.deleted_at IS NULL
+   AND subj.code_translation = 'English Language Arts'
+   AND a.academic_year = 2017
+   -- KAS
+   AND u.user_id = 309
+ORDER BY administered_at DESC;
---
+++ assess/assess$test_prep_math.sql
@@ -0,0 +1,26 @@
+DROP VIEW assess$test_prep_math$assessments;
+
+CREATE VIEW assess$test_prep_math$assessments AS
+SELECT a.assessment_id
+      ,a.academic_year
+      ,a.title
+      ,a.administered_at
+      ,a.tags
+      ,a.performance_band_set_id
+      ,subj.code_translation AS subject_area
+      ,scope.code_translation AS scope
+      ,u.state_id
+  FROM il_dna_assessments.assessments a
+  JOIN il_public.users u
+    ON a.user_id = u.user_id
+  LEFT OUTER JOIN il_codes.dna_subject_areas subj
+    ON a.code_subject_area_id = subj.code_id
+  LEFT OUTER JOIN il_codes.dna_scopes scope
+    ON a.code_scope_id = scope.code_id
+ WHERE a.deleted_at IS NULL
+   AND subj.code_translation = 'Mathematics'
+   AND a.academic_year = 2017
+   -- KAS
+   --AND u.user_id = 309
+   AND a.title LIKE '2016-17 Math Test Prep%'
+ORDER BY a.title ASC;
